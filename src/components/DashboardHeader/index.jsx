0cafca35fb853b19df85ce36fe27d78b79be6b1f
naufaladrian <naufal.adrian904@gmail.com>
2023-12-13 22:29:36 +0700

    add meta tags, and fill alt img

--- src/components/DashboardHeader/index.jsx
+++ src/components/DashboardHeader/index.jsx
@@ -17,10 +17,10 @@ export default function index() {
     }, []);
     return (
         <section id='home' className='px-[1rem] md:px-[8rem] w-full min-h-screen relative flex items-center justify-center  '  >
-            <img src={DepanResto} className='faded-image w-full absolute h-screen md:max-h-screen -z-[2] object-cover' />
-            <img src={DalamResto} className='faded-image w-full absolute h-screen md:max-h-screen -z-[2] object-cover' />
-            <img src={DalamResto2} className='faded-image w-full absolute h-screen md:max-h-screen -z-[2] object-cover' />
-            <img src={KasirResto} className='faded-image w-full absolute h-screen md:max-h-screen -z-[2] object-cover' />
+            <img src={DepanResto} className='faded-image w-full absolute h-screen md:max-h-screen -z-[2] object-cover' alt='bg' />
+            <img src={DalamResto} className='faded-image w-full absolute h-screen md:max-h-screen -z-[2] object-cover' alt='bg' />
+            <img src={DalamResto2} className='faded-image w-full absolute h-screen md:max-h-screen -z-[2] object-cover' alt='bg' />
+            <img src={KasirResto} className='faded-image w-full absolute h-screen md:max-h-screen -z-[2] object-cover' alt='bg' />
             <div className=' absolute w-full min-h-screen bg-black -z-[1] opacity-70'></div>
             <div className=' text-white text-center font-crimson border-[6px] border-white border-double py-24 px-3 md:p-16' data-aos="fade-up">
                 <h1 className=' font-[600] text-6xl md:text-8xl mb-4  md:leading-[6rem]'>Soto Ayam Dargo <br />Pak Wito</h1>
