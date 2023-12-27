c1ffe65deeb17fdee9d94383b0ce926ddcb22b4f
naufaladrian <naufal.adrian904@gmail.com>
2023-12-27 22:11:50 +0700

    update menu and animation

--- src/components/Facility/index.jsx
+++ src/components/Facility/index.jsx
@@ -1,7 +1,6 @@
-import React from 'react'
 import MeetingRoom from '../../assets/images/meeting-room.jpg'
 import Wifi from '../../assets/images/wifi.jpg'
-import DalamResto from '../../assets/images/dalam-resto.jpg'
+import Parkir from '../../assets/images/parkir.jpg'
 import Card from '../CardFacility'
 
 export default function index() {
@@ -15,15 +14,15 @@ export default function index() {
             facilityName: 'Free WiFi'
         },
         {
-            facilityImg: DalamResto,
-            facilityName: 'Tempat Nyaman'
+            facilityImg: Parkir,
+            facilityName: 'Parkir Luas'
         },
     ]
     return (
         <section id='fasilitas' className='w-full px-[1rem] md:px-[8rem] py-8 pt-16 font-poppins bg-white text-center'>
             <h2 className='text-brown2 font-[600]  md:text-5xl text-4xl w-fit mx-auto mb-6 border-b-4 py-3 px-2 border-brown3'>Fasilitas</h2>
             <div className=' flex mx-auto items-center justify-center md:justify-between gap-y-8 flex-wrap flex-row '>
-                {facilities.map((facility, index) => (
+                {facilities.map((facility) => (
                     <Card facilityImg={facility.facilityImg} facilityName={facility.facilityName} />
                 ))}
 
--- src/components/FavMenu/index.jsx
+++ src/components/FavMenu/index.jsx
@@ -1,18 +1,37 @@
 import React from 'react'
 import CardMenu from '../CardMenu'
 import Soto from '../../assets/images/soto1.jpg'
+import Uwuh from '../../assets/images/uwuh.jpg'
+import Sate from '../../assets/images/sate.jpg'
 
 export default function index() {
+    const menus = [
+        {
+            menuImg: Uwuh,
+            menuName: 'Wdang Uwuh',
+            menuPrice: 'Rp 13.000'
+        },
+        {
+            menuImg: Soto,
+            menuName: 'Soto Jumbo',
+            menuPrice: 'Rp 18.000'
+        },
+        {
+            menuImg: Sate,
+            menuName: 'Aneka Sate',
+            menuPrice: 'Rp 9.000'
+        },
+    ]
 
     return (
-        <section id='menu' className='w-full px-[1rem] md:px-[8rem] py-8 font-poppins bg-white text-center'>
+        <section id='menu' className='w-full px-[1rem] md:px-[8rem] py-16 font-poppins bg-white text-center'>
             <h2 className='text-brown2 font-[700] leading-[55px] md:text-5xl text-4xl border-b-4 w-fit mx-auto py-3 border-brown3'>Menu Terlaris</h2>
-            <div className=' flex mx-auto items-center justify-center gap-x-4 flex-wrap flex-row  '>
-                {[1, 2, 3].map((item, index) => (
-                    <CardMenu img={Soto} />
+            <div className=' flex mx-auto items-center justify-center gap-x-4 flex-wrap flex-row mb-12'>
+                {menus.map((item, index) => (
+                    <CardMenu img={item.menuImg} menu={item.menuName} harga={item.menuPrice} />
                 ))}
-
             </div>
+            <a href="https://heyzine.com/flip-book/4d5e291245.html" className=' bg-brown2 text-brown4 font-crimson text-3xl rounded-md px-6 py-4' target="_blank">Daftar Menu</a>
         </section>
     )
 }
